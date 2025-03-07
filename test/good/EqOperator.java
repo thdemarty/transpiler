eef94cf2e815ee1044c39b7ff03496b53b5f0e0f
thdemarty <48022911+thdemarty@users.noreply.github.com>
2025-03-07 09:56:16 +0100

    Eq operator: better testing

--- test/good/EqOperator.java
+++ test/good/EqOperator.java
@@ -1,11 +1,45 @@
 class EqOperator {
     public static void main(String[] a) {
         {
-            if (true == true || 1 == 1) {
-                System.out.println(1);
-            } else {
-                System.out.println(0);
-            }
+            System.out.println(new Test().testAll());
         }
+    }
+}
+
+
+
+class Test {
+    DummyClass d1;
+    DummyClass d2;
+    int res;
+
+    public int testAll() {
+        int res;
+        res = this.testClass();
+        return 0;
+    }
+
+    public int testClass() {
+        d1 = new DummyClass();
+        d2 = new DummyClass();
+        
+        res = d1.setV(1);
+        res = d2.setV(1);
+        
+        if (d1 == d2) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+        return 0;
+    }
+}
+
+class DummyClass {
+    int value;
+
+    public int setV(int v) {
+        value = v;
+        return 0;
     }
 }
